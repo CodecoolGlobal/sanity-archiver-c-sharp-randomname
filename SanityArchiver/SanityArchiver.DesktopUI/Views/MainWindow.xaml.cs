3ea9cfaef0d0603319515556ac95f68c9136ba71
kruppatomi <hungaryca@gmail.com>
2019-10-01 15:22:36 +0200

    treeview alnd listbox

--- SanityArchiver/SanityArchiver.DesktopUI/Views/MainWindow.xaml.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/MainWindow.xaml.cs
@@ -1,18 +1,60 @@
-﻿using System.Windows;
-
-namespace SanityArchiver.DesktopUI.Views
+﻿namespace SanityArchiver.DesktopUI.Views
 {
+    using System;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Controls;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private object dummyNode = null;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = s;
+                item.Tag = s;
+                item.FontWeight = FontWeights.Normal;
+                item.Items.Add(dummyNode);
+                item.Expanded += new RoutedEventHandler(folder_Expanded);
+                foldersItem.Items.Add(item);
+            }
+        }
+
+        void folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            if (item.Items.Count == 1 && item.Items[0] == dummyNode)
+            {
+                item.Items.Clear();
+                try
+                {
+                    foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+                    {
+                        TreeViewItem subitem = new TreeViewItem();
+                        subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+                        subitem.Tag = s;
+                        subitem.FontWeight = FontWeights.Normal;
+                        subitem.Items.Add(dummyNode);
+                        subitem.Expanded += new RoutedEventHandler(folder_Expanded);
+                        item.Items.Add(subitem);
+                    }
+                }
+                catch (Exception) {}
+            }
         }
     }
 }
